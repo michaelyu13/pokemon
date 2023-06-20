170997274a4457e4444d596bdee261966c05b160
Michael Yu <michael@michaelyu.co.uk>
2023-06-21 00:47:45 +0100

    Filter - Add button to clear search

--- src/Components/FilterAndSort/Search.jsx
+++ src/Components/FilterAndSort/Search.jsx
@@ -2,25 +2,37 @@ import React, { useContext } from 'react';
 import { PokemonContext } from '../../App';
 
 const Search = () => {
-    const { setSearchInput } = useContext(PokemonContext);
+    const { searchInput, setSearchInput } = useContext(PokemonContext);
 
     const handleSearch = (e) => {
         setSearchInput(e.target.value);
     };
 
+    const handleClearSearch = () => {
+        setSearchInput('');
+    };
+
     return (
-        <div>
+        <div className="relative">
             <label className="mr-2 font-bold" htmlFor="search">
                 Search:
             </label>
             <input
-                className="border-styles h-8 w-64 px-2"
+                className="border-styles h-8 w-64 pl-2 pr-10"
                 onChange={handleSearch}
                 type="text"
                 name="search"
                 id="search"
+                value={`${searchInput}`}
                 placeholder="Pokémon Name"
             />
+            <button
+                onClick={handleClearSearch}
+                className={`${searchInput ? 'visible opacity-100' : 'invisible opacity-0'}
+                absolute right-0 top-1 mr-2 flex h-6 w-6 items-center justify-center rounded-full bg-slate-200 transition-colors transition-opacity duration-500 hover:bg-slate-400`}
+            >
+                <span className="mb-px text-white">&times;</span>
+            </button>
         </div>
     );
 };
